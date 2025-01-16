9502c0419cf0abc46c3f4bbfc94c897b5b36c7db
kykit <kykitos@yandex.ru>
2025-01-17 00:16:30 +0300

    fix main

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func CreateStatsAnalyzer(loadThreshold, memoryThreshold, diskThreshold, networkT
 			fmt.Printf("Free disk space is too low: %d Mb left\n", availableSpace)
 		}
 		if networkUsagePercent > networkThreshold {
-			availableBandwidth := (stats.NetworkCapacity - stats.NetworkUsage) * 8 / 1_000_000
+			availableBandwidth := (stats.NetworkCapacity - stats.NetworkUsage) * 8 / (1024 * 1024)
 			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", availableBandwidth)
 		}
 	}
